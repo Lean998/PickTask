52663958de23ba476a522d1e14a4adb73850b767
Lean998 <leandroaguero83@gmail.com>
2025-05-31 20:40:04 -0300

    testing unitario

---
+++ tests/unit/TareaTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use CodeIgniter\Test\FeatureTestTrait;
+use PHPUnit\Framework\TestCase;
+use App\Models\Tarea;
+    class TareaTest extends TestCase {
+    use FeatureTestTrait;
+
+    public function testGuardarTareaConDatosValidos(){
+        //Inicializar el modelo
+        $tarea = new Tarea();
+
+        //Datos validos para una tarea
+        $data = [
+            'tarea_id' => 1,
+            'titulo' => 'Tarea test',
+            'descripcion' => 'Desde test',
+            'prioridad' => 'normal',
+            'fecha_vencimiento' => '2025-12-31',
+            'fecha_recordatorio' => '2025-12-01',
+            'color' => 'azul',
+        ];
+        
+        //Verificar que los datos sean validos
+        //Retornos True (validos) False (Invalidos)
+        $result = $tarea->validate($data);
+
+        //Verificar que el resultado obtenido sea True (Valido)
+        $this->assertTrue($result);
+        
+    }
+
+    public function testGuardarTareaConDatosInvalidos(){
+        //Inicializar el modelo
+        $tarea = new Tarea();
+
+        //Preparacion de datos invalidos
+        $data = [
+            'tarea_id' => 1,
+            'titulo' => '',
+            'descripcion' => '',
+            'prioridad' => '',
+            'fecha_vencimiento' => '',
+            'fecha_recordatorio' => '',
+            'color' => '',
+        ];
+
+        //Verificar que los datos sean validos
+        //Retornos True (validos) False (Invalidos)
+        $result = $tarea->validate($data);
+        
+        //Verificar que el resultado obtenido sea false
+        $this->assertFalse($result);
+
+        //Almacenar los mensajes de error
+        $errors = $tarea->errors();
+
+        //Verificar que los campos obligatorios esten señalados como invalidos/con errores. 
+        $this->assertArrayHasKey('titulo', $errors);
+        $this->assertArrayHasKey('prioridad', $errors);
+    }
+
+}
---
+++ tests/unit/UsuarioTest.php
@@ -0,0 +1,109 @@
+<?php
+
+use CodeIgniter\Test\FeatureTestTrait;
+use PHPUnit\Framework\TestCase;
+use Config\Services;
+    class UsuarioTest extends TestCase {
+    use FeatureTestTrait;
+    
+    public function testGuardarUsuarioValido(){
+    //Declaracion de los datos
+    $data = [
+        'nombre' => "John",
+        'correo' => 'nuevo@email.com',
+        'contrasenia' => 'John123',
+    ];
+    //Declaracion de las reglas
+    $reglas = [
+        'nombre' => 'required',
+        'correo' => 'required|valid_email',
+        'contrasenia' => 'required'
+    ];
+
+    $validation = Services::validation();
+    $validation->setRules($reglas);
+
+    //Esperamos que sea True (Datos validos)
+    $this->assertTrue($validation->run($data));
+}
+
+public function testGuardarUsuarioInvalido(){
+    //Declaracion de los datos
+    $data = [
+        'nombre' => "John",
+        'correo' => 'nuevo@email.com',
+        'contrasenia' => 'John',
+    ];
+    
+    //Declaracion de las reglas
+    $reglas = [
+        'nombre' => 'required|min_length[5]',
+        'correo' => 'required|valid_email',
+        'contrasenia' => 'required|min_length[8]'
+    ];
+
+    $validation = Services::validation();
+    $validation->setRules($reglas);
+
+    //Esperamos que sea False (datos invalidos)
+    $this->assertFalse($validation->run($data));
+}
+    public function testGuardarUsuarioConCorreoExistente(){
+        
+        //Lista de correos existentes. 
+        $correoExistente = [
+            ['correo' => 'john89@gmail.com'],
+            ['correo' => 'usuario@example.com']
+        ];
+
+        //Declaracion de los datos a insertar
+        $data = [
+            'nombre' => "John",
+            'correo' => 'john89@gmail.com',
+            'contrasenia' => 'John123',
+        ];
+
+        //Declaracion de las reglas de validacion
+        $rules = [
+            'nombre' => 'required',
+            'correo' => 'required|valid_email',
+            'contrasenia' => 'required'
+        ];
+        
+        $validation = Services::validation();
+        $validation->reset(); 
+        $validation->setRules($rules);
+
+        // Validacion basica de los datos
+        $this->assertTrue($validation->run($data));
+
+        // Obtener correos atraves de la clave
+        $correos = array_column($correoExistente, 'correo');
+
+        // Esperamos que sea True (El correo ya existe.)
+        $this->assertTrue(in_array($data['correo'], $correos));
+    }
+
+    public function testGuardarUsuarioConContraseniaInvalida(){
+        
+
+        //Declaracion de los datos a insertar
+        $data = [
+            'nombre' => "John",
+            'correo' => 'john89@gmail.com',
+            'contrasenia' => 'John123',
+        ];
+
+        //Declaracion de las reglas de validacion
+        $rules = [
+            'contrasenia' => 'required|min_length[8]'
+        ];
+        
+        $validation = Services::validation();
+        $validation->reset(); 
+        $validation->setRules($rules);
+
+        // Validacion basica de los datos
+        $this->assertFalse($validation->run($data));
+    }
+}
